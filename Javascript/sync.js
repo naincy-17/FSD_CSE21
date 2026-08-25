bc6c3e433b10249e5a9842f204fe4dd1d7df7d54
naincy-17 <naincymaurya17@gmail.com>
2026-08-25 09:42:02 +0530

    updated

--- Javascript/sync.js
+++ Javascript/sync.js
@@ -1,9 +1,9 @@
 //Synchronous code
-// console.log("task one");
-// for (let i = 1; i <= 1000000; i++) {
-//     console.log(i);
-// }
-// console.log("task two");
-// console.log("task three");
-// console.log("hi!!!  ABES");
+console.log("task one");
+for (let i = 1; i <= 1000000; i++) {
+    console.log(i);
+}
+console.log("task two");
+console.log("task three");
+console.log("hi!!!  ABES");
 
