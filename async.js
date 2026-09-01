10a8c2b5d9f7ece2757916d2e597fffa6442c00e
naincy-17 <naincymaurya17@gmail.com>
2026-09-01 09:34:11 +0530

    reduce

---
+++ async.js
@@ -0,0 +1,9 @@
+//async code
+console.log("task one");
+console.log("task two");
+setTimeout(() => {
+    console.log("task three");
+}, 200000)
+
+console.log("task four");
+console.log("task five");
